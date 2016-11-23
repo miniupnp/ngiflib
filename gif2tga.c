4cb62e749bf9d62fac402137cf79d5f80e9c52a7
Thomas Bernard <miniupnp@free.fr>
2016-11-23 17:36:44 +0100

    better command line handling for gif2tga

--- gif2tga.c
+++ gif2tga.c
@@ -13,23 +13,48 @@ int main(int argc, char * * argv) {
 	struct ngiflib_img * img;
 	int err, i;
 	char tganame[256];
-	
-	gif = (struct ngiflib_gif *)malloc(sizeof(struct ngiflib_gif));
-	memset(gif, 0, sizeof(struct ngiflib_gif));
+	const char * input_file = NULL;
+	int indexed = 0;
+	const char * output_dir = ".";
 	
 	if(argc<2) {
-		printf("Usage: machin truc.gif [indexed]\n");
-		free(gif);
+		printf("Usage: machin [--indexed|-i] [--outdir dir] truc.gif\n");
 		return 1;
 	}
-	fgif = fopen(argv[1], "rb");
+	for(i=1; i < argc; i++) {
+		if(argv[i][0] == '-') {
+			if(strcmp(argv[i], "--indexed") == 0
+			   || strcmp(argv[i], "-i") == 0) {
+				indexed = 1;
+			} else if(strcmp(argv[i], "--outdir") == 0) {
+				if(++i >= argc) {
+					fprintf(stderr, "option %s need one argument.\n", argv[i - 1]);
+					return 2;
+				}
+				output_dir = argv[i];
+			} else {
+				fprintf(stderr, "%s: unknown option.\n", argv[i]);
+				return 2;
+			}
+		} else if(input_file == NULL) {
+			input_file = argv[i];
+		} else {
+			fprintf(stderr, "%s: invalid argument.\n", argv[i]);
+			return 2;
+		}
+	}
+
+	gif = (struct ngiflib_gif *)malloc(sizeof(struct ngiflib_gif));
+	memset(gif, 0, sizeof(struct ngiflib_gif));
+
+	fgif = fopen(input_file, "rb");
 	if(fgif==NULL) {
 		printf("Cannot open %s\n",argv[1]);
-		return 2;
+		return 3;
 	}
 	gif->input = (void *)fgif;
 	gif->mode = NGIFLIB_MODE_FROM_FILE;
-	if(argc>2) {
+	if(indexed) {
 		gif->mode |= NGIFLIB_MODE_INDEXED;
 		printf("INDEXED MODE\n");
 	} else gif->mode |= NGIFLIB_MODE_TRUE_COLOR;
@@ -40,17 +65,20 @@ int main(int argc, char * * argv) {
 	
 	  if(err==1) {
 	  	int localpalsize;
-		char * dot;
+		char * p;
 		img = gif->cur_img;
 		localpalsize = 1 << img->localpalbits;
-		strncpy(tganame, argv[1], sizeof(tganame));
-		dot = strrchr(tganame, '.');
-		if(dot != NULL) {
-			sprintf(dot, "_out%02d.tga", gif->nimg);
-		} else {
-			sprintf(tganame, "%s_out%02d.tga", argv[1], gif->nimg);
+		snprintf(tganame, sizeof(tganame), "%s/%s", output_dir, input_file);
+		p = strrchr(tganame, '.');
+		if(p == NULL) {
+			p = tganame + strlen(tganame);
 		}
+		sprintf(p, "_out%02d.tga", gif->nimg);
 		ftga = fopen(tganame, "wb");
+		if(ftga == NULL) {
+			fprintf(stderr, "cannot open file %s for writing.\n", tganame);
+			return 5;
+		}
 		putc(0, ftga);	//0
 		if(gif->mode & NGIFLIB_MODE_INDEXED) {
 			putc(1, ftga);// With palette
