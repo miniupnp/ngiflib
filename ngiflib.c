3103d071e5808aa5a0cd9a882a7121e91d5490c6
Thomas Bernard <miniupnp@free.fr>
2017-12-15 16:10:23 +0100

    let GetByteStr() return void...

--- ngiflib.c
+++ ngiflib.c
@@ -90,19 +90,19 @@ static u16 GetWord(struct ngiflib_gif * g) {
  * et le nombre d'octet a lire.
  * Renvoie 0 si l'operation a reussi, -1 sinon.
  */
-static int GetByteStr(struct ngiflib_gif * g, u8 * p, int n) {
-	if(!p) return -1;
+static void /*int*/ GetByteStr(struct ngiflib_gif * g, u8 * p, int n) {
+	/*if(!p) return -1;*/
 #ifndef NGIFLIB_NO_FILE
 	if(g->mode & NGIFLIB_MODE_FROM_MEM) {
 #endif /* NGIFLIB_NO_FILE */
 		memcpy(p, g->input.bytes, n);
 		g->input.bytes += n;
-		return 0;
+		/*return 0;*/
 #ifndef NGIFLIB_NO_FILE
 	} else {
-		size_t read;
-		read = fread(p, 1, n, g->input.file);
-		return ((int)read == n) ? 0 : -1;
+		/*size_t read;
+		read =*/ fread(p, 1, n, g->input.file);
+		/*return ((int)read == n) ? 0 : -1;*/
 	}
 #endif /* NGIFLIB_NO_FILE */
 }
