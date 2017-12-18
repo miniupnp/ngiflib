c4f737606e7c79663a094305b27a18d92b775586
Thomas Bernard <miniupnp@free.fr>
2017-12-18 11:03:16 +0100

    small fix in FillGifBackGround()

--- ngiflib.c
+++ ngiflib.c
@@ -367,8 +367,6 @@ static void FillGifBackGround(struct ngiflib_gif * g) {
 	if((g->frbuff.p8==NULL)||(g->palette==NULL)) return;
 #ifndef NGIFLIB_INDEXED_ONLY
 	if(g->mode & NGIFLIB_MODE_INDEXED) {
-#else
-	{
 #endif /* NGIFLIB_INDEXED_ONLY */
 		ngiflib_memset(g->frbuff.p8, g->backgroundindex, n);
 #ifndef NGIFLIB_INDEXED_ONLY
@@ -376,8 +374,8 @@ static void FillGifBackGround(struct ngiflib_gif * g) {
 		u32 * p = g->frbuff.p32;
 		bg_truecolor = GifIndexToTrueColor(g->palette, g->backgroundindex);
 		while(n-->0) *p++ = bg_truecolor;
-#endif /* NGIFLIB_INDEXED_ONLY */
 	}
+#endif /* NGIFLIB_INDEXED_ONLY */
 }
 
 /* ------------------------------------------------ */
