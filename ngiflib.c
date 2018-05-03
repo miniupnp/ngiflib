93d864a94d7644a65ff64bdb1da44d6ac25c90ef
Thomas Bernard <miniupnp@free.fr>
2018-05-03 17:42:02 +0200

    GifDestroy(): call GifImgDestroy() before freeing the palette
    see #2

--- ngiflib.c
+++ ngiflib.c
@@ -53,9 +53,9 @@ void fprintf_ngiflib_gif(FILE * f, struct ngiflib_gif * g) {
 
 void GifDestroy(struct ngiflib_gif * g) {
 	if(g==NULL) return;
+	GifImgDestroy(g->first_img);
 	if(g->palette) ngiflib_free(g->palette);
 	if(g->frbuff.p8) ngiflib_free(g->frbuff.p8);
-	GifImgDestroy(g->first_img);
 	ngiflib_free(g);
 }
 
