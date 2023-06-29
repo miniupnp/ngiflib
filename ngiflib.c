miniupnp/ngiflib

commit cf429e0a2fe26b5f01ce0c8e9b79432e94509b6e

fix "pixel overrun"
fixes #3

ngiflib.c
@@ -528,7 +528,8 @@ static int DecodeGifImg(struct ngiflib_img * i) {
 			act_code = GetGifWord(i, &context);
 			casspecial = (u8)act_code;
 			old_code = act_code;
-			WritePixel(i, &context, casspecial); npix--;
+			if(npix > 0) WritePixel(i, &context, casspecial);
+			npix--;
 		} else {
 			read_byt = act_code;
 			if(act_code >= free) {	/* code pas encore dans alphabet */
@@ -545,7 +546,11 @@ static int DecodeGifImg(struct ngiflib_img * i) {
 			/* act_code est concret */
 			casspecial = (u8)act_code;	/* dernier debut de chaine ! */
 			*(--stackp) = casspecial;	/* push on stack */
-			WritePixels(i, &context, stackp, stack_top - stackp);	/* unstack all pixels at once */
+			if(npix >= (stack_top - stackp)) {
+				WritePixels(i, &context, stackp, stack_top - stackp);	/* unstack all pixels at once */
+			} else if(npix > 0) {	/* "pixel overflow" */
+				WritePixels(i, &context, stackp, npix);
+			}
 			npix -= (stack_top - stackp);
 			stackp = stack_top;
 /*			putchar('\n'); */

commit fb2713a548a530c13f14b586a479818cb0182a5e

fix npix counting
bug introduced in cf429e0a2fe26b5f01ce0c8e9b79432e94509b6e
fixes #25

ngiflib.c
@@ -585,8 +585,10 @@ static int DecodeGifImg(struct ngiflib_img * i) {
 			}
 			casspecial = (u8)act_code;
 			old_code = act_code;
-			if(npix > 0) WritePixel(i, &context, casspecial);
-			npix--;
+			if(npix > 0) {
+				WritePixel(i, &context, casspecial);
+				npix--;
+			}
 		} else if(act_code > free) {
 #if !defined(NGIFLIB_NO_FILE)
 			if(i->parent && i->parent->log) fprintf(i->parent->log, "Invalid code %hu (free=%hu) !\n", act_code, free);
@@ -610,10 +612,11 @@ static int DecodeGifImg(struct ngiflib_img * i) {
 			*(--stackp) = casspecial;	/* push on stack */
 			if(npix >= (stack_top - stackp)) {
 				WritePixels(i, &context, stackp, stack_top - stackp);	/* unstack all pixels at once */
+				npix -= (stack_top - stackp);
 			} else if(npix > 0) {	/* "pixel overflow" */
 				WritePixels(i, &context, stackp, npix);
+				npix = 0;
 			}
-			npix -= (stack_top - stackp);
 			stackp = stack_top;
 /*			putchar('\n'); */
 			if(free < 4096) { /* la taille du dico est 4096 max ! */